0dcc43e58a60369c2277d47342fdda1f5d73229b
Krow <katie.scantland@stu.glasgow.kyschools.us>
2024-09-06 08:51:50 -0500

    Initial Commit

---
+++ Checkpoint 6.playground/Contents.swift
@@ -0,0 +1,25 @@
+import Cocoa
+
+struct Car {
+    var carModel = "blue"
+    var numberOfSeats = 5
+    var shiftCurrentGear = Int.random(in: 1...10)
+    func value() { if (shiftCurrentGear != 0){                    if (shiftCurrentGear != 0)) { print ("Cannot Start") }
+    }
+                       
+                         
+ ; else {
+ }
+    int(func shiftCurrentGear() {
+        if shiftCurrentGear is < 10; do {
+            print ("Please turn vehicle off")
+        }; if; shiftCurrentGear = 1...10
+        print ("Car Started! Please Drive Safe!")
+        If else
+            print ("turn vehicle off")
+        }
+        
+}
+        
+        
+    }
